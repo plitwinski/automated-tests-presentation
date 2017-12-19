fd841197c5891dd2ac21b7f593d930b242bfebb6
Piotr Litwinski <piotr.litwinski@hotmail.com>
2017-12-19 22:38:06 +0100

    Adjust path name to make jest work.

---
+++ examples/03 - local-headless json-server/src/webdriver.test.js
@@ -0,0 +1,7 @@
+import webdriver from 'selenium-webdriver'
+import { FirefoxProfile } from 'firefox-profile'
+
+test('make sure movies\' list has been rendered', () => {
+    console.log('TEST')
+    expect(true).toBe(true);
+});
