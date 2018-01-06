0aef32fc4eee4ae41d1bb95b554e23fb3263cfd7
Piotr Litwinski <piotr.litwinski@hotmail.com>
2018-01-06 22:29:47 +0100

    Add example 4 - scenario testing.

---
+++ examples/04 - testing-scenarios/Example.TestingScenarios.Tests/TestingScenarioBase.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System.Threading.Tasks;
+
+namespace Example.TestingScenarios.Tests
+{
+    public abstract class TestingScenarioBase
+    {
+        [OneTimeSetUp]
+        public async Task OneTimeSetup()
+        {
+            await Given();
+            await When();
+        }
+
+        public virtual Task Given()
+        {
+            return Task.CompletedTask;
+        }
+
+        public abstract Task When();
+    }
+}
