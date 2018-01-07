12b394e76b082df5aeb35d4452275882efdafd6f
Piotr Litwinski <piotr.litwinski@hotmail.com>
2018-01-07 14:44:09 +0100

    Add fixture to example 5.

---
+++ examples/05 - modularity/Example.Modularity.Tests/Controllers/Movies/Fixtures/HttpClientFixture.cs
@@ -0,0 +1,40 @@
+﻿using Example.InMemoryDependencies.Core;
+using Example.Modularity.Tests.Factories;
+using Moq;
+using System.Net.Http;
+
+namespace Example.Modularity.Tests.Controllers.Movies.Fixtures
+{
+    public class HttpClientFixture
+    {
+        private readonly ServerFactory serverFactory;
+
+        public Mock<IQueueClient> QueueClientMock { get; private set; }
+
+        public HttpClientFixture()
+        {
+            serverFactory = new ServerFactory();
+        }
+
+        public HttpClientFixture AddMovies()
+        {
+            var context = new MoviesContextFactory("databaseNameFixture")
+                .AddEntities(Data.Movie())
+                .Create();
+            serverFactory.AddMock(context);
+            return this;
+        }
+
+        public HttpClientFixture AddQueueClient()
+        {
+            QueueClientMock = new Mock<IQueueClient>();
+            serverFactory.AddMock(QueueClientMock.Object);
+            return this;
+        }
+
+        public HttpClient Create()
+        {
+            return serverFactory.Create().CreateClient();
+        }
+    }
+}
---
+++ examples/05 - modularity/Example.Modularity.Tests/Controllers/Movies/WhenUpdatesArrivedWithFixture.cs
@@ -0,0 +1,68 @@
+﻿using Example.InMemoryDependencies.Messages;
+using Example.InMemoryDependencies.Models;
+using Example.Modularity.Tests.Controllers.Movies.Fixtures;
+using Moq;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Example.Modularity.Tests.Controllers.Movies
+{
+    public class WhenUpdatesArrivedWithFixture : ScenarioTestingBase
+    {
+        private const string Cinema1 = "Cinema1";
+        private const string Cinema2 = "Cinema2";
+        
+        private CinemaUpdate[] _cinemaUpdates;
+        private HttpClientFixture _clientFixture;
+        private HttpClient _client;
+
+        public override Task Given()
+        {
+            _clientFixture = new HttpClientFixture()
+                .AddMovies()
+                .AddQueueClient();
+
+            _client = _clientFixture.Create();
+            return Task.CompletedTask;
+        }
+
+
+        public override async Task When()
+        {
+            var result = await _client.GetAsync("api/movieUpdates");
+            _cinemaUpdates = JsonConvert.DeserializeObject<CinemaUpdate[]>(await result.Content.ReadAsStringAsync());
+        }
+
+        [Test]
+        public void ThenCinema1IsReturned()
+        {
+            Assert.That(_cinemaUpdates[0].Name, Is.EqualTo(Cinema1));
+        }
+
+        [Test]
+        public void ThenCinema1MoviesCollectionIsNotEmpty()
+        {
+            Assert.IsNotEmpty(_cinemaUpdates[0].AddedMovies);
+        }
+
+        [Test]
+        public void ThenCinema2IsReturned()
+        {
+            Assert.That(_cinemaUpdates[1].Name, Is.EqualTo(Cinema2));
+        }
+
+        [Test]
+        public void ThenCinema2MoviesCollectionIsNotEmpty()
+        {
+            Assert.IsNotEmpty(_cinemaUpdates[1].AddedMovies);
+        }
+
+        [Test]
+        public void ThenTwoEventsWerePublished()
+        {
+            _clientFixture.QueueClientMock.Verify(p => p.PublishMessageAsync(It.IsAny<MovieAddedMessage>()), Times.Exactly(2));
+        }
+    }
+}
